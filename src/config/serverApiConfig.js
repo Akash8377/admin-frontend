fb640c8326edddc7e9c673b64c444d4c9cbc2596
Akash8377 <121550790+Akash8377@users.noreply.github.com>
2023-08-18 19:53:04 +0530

    Update serverApiConfig.js

--- src/config/serverApiConfig.js
+++ src/config/serverApiConfig.js
@@ -1,9 +1,9 @@
 export const API_BASE_URL =
   process.env.NODE_ENV == 'production' || process.env.REACT_APP_DEV_REMOTE == 'remote'
-    ? 'https://dev-server.idurarapp.com/api/'
-    : 'https://admin-panell.onrender.com/api/';
+    ? 'https://admin-panell.onrender.com/api'
+    : 'https://adminss-panel.onrender.com/api/';
 export const DOWNLOAD_BASE_URL =
   process.env.NODE_ENV == 'production' || process.env.REACT_APP_DEV_REMOTE == 'remote'
-    ? 'https://dev-server.idurarapp.com/download/'
-    : 'https://admin-panell.onrender.com/download/';
+    ? 'https://admin-panell.onrender.com/download'
+    : 'https://adminss-panel.onrender.com/download/';
 export const ACCESS_TOKEN_NAME = 'x-auth-token';
